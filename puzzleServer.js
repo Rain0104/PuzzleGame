ddfc22452da8424e524c3e5a900cde5a13fdef2f
Rain0104 <b1ack_rain@mail.ru>
2016-10-06 23:56:00 +0300

    Updated puzzle server added 500 and 404 pages

--- puzzleServer.js
+++ puzzleServer.js
@@ -3,6 +3,7 @@
  */
 var express = require('express');
 var fs = require('fs');
+var pug = require('pug');
 var multer = require('multer');
 var config = require('./config');
 var mongoose = require('mongoose');
@@ -18,18 +19,25 @@ var allowCrossDomain = function (req, res, next) {
 
 //setup express application
 var app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(allowCrossDomain);
-
 app.use('/', express.static(__dirname + '/public'));
+
+
 //todo save images in public directory
-//app.use('/css', express.static(__dirname + '/public/css'));
-//app.use('/js', express.static(__dirname + '/public/js'));
-//app.use('/libs', express.static(__dirname + '/public/libs'));
-//app.use('/images', express.static(__dirname + '/public/images'));
+
 app.use('/commonPuzzleImages', express.static(__dirname + '/resources/commonPuzzleImages'));
 app.use('/images', express.static(__dirname + '/resources/images'));
+
+app.set('view engine', 'pug');
+
+//
+app.get('/pug', function (req, res) {
+    res.render(__dirname + '/public/views/main-layout.pug', {title: 'Puzzle Game', message: 'Hello there!'});
+});
+//
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
@@ -52,10 +60,11 @@ var pathForImages = __dirname + '/resources/images/';
 //
 //newImage.save(function (err) {
 //    if (err) throw err;
-//    console.log('Image created!');
+//    console.log('Image was created!');
 //    //res.statusCode = 200;
-//    //res.send({success: 'New Image added!'});
+//    //res.send({success: 'New Image was added!'});
 //});
+
 
 app.get('/api/commonImages', function (reg, res) {
     Image.find({type: 'common'}, function (err, image) {
@@ -104,6 +113,8 @@ app.post('/api/signUp', function (req, res) {
         }
     });
 });
+
+
 app.post('/api/updateLeaderBoard', function (req, res) {
     var imageName = req.body.imageName;
     var username = req.body.username;
@@ -115,22 +126,23 @@ app.post('/api/updateLeaderBoard', function (req, res) {
     Image.findOne({name: imageName}, function (err, image) {
         if (err) throw err;
         if (image === undefined) {
-            console.log('no find', image);
-            res.send({Warning: 'Not image find'});
+            console.log('Image not found', image);
+            res.send({Warning: 'Image not found'});
 
         } else {
             var updatedLBoard = image.updateLBoard(username, time);
             if (updatedLBoard.length !== 0) {
                 image.leader_board = updatedLBoard;
-                image.save(function (err) { });
+                image.save(function (err) {
+                });
             }
             res.send({Success: 'LeaderBoard updated'});
         }
     });
 });
 
-app.get('/api/getLeaderBoard/:name', function(req, res) {
-    var imageName =req.params.name;
+app.get('/api/getLeaderBoard/:name', function (req, res) {
+    var imageName = req.params.name;
     Image.findOne({name: imageName}, function (err, image) {
         if (err) throw err;
         res.send(image.leader_board);
@@ -139,7 +151,7 @@ app.get('/api/getLeaderBoard/:name', function(req, res) {
 
 app.get('/api/getPlayerImages/:player', function (req, res) {
     var player = req.params.player;
-    console.log ('get images', player);
+    console.log('get images', player);
     Image.find({type: player + 'Image'}, function (err, image) {
         if (err) throw err;
 
@@ -193,12 +205,25 @@ app.post('/api/addImage', upload.single('file'), function (req, res) {
 
                 newImage.save(function (err) {
                     if (err) throw err;
-                    console.log({Success: 'Image created!'});
+                    console.log({Success: 'Image is created!'});
                 });
                 res.send({Success: 'Image is added'});
             });
         }
     });
+});
+
+app.use(function (req, res) {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not found');
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server error');
 });
 
 
